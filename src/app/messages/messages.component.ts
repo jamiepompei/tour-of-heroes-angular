a5a756a0956cd2e69c0f31083719b12cabe80fb2
nicolepompei <nicole.lynn.pompei@gmail.com>
2020-12-31 18:52:00 -0500

    injected messages service into messagescomponent and added messageservice prop to messagecomponent constructor

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
+//messagesComponent should display all messages, including the messages sent by the HeroService when it fetches heroes
 export class MessagesComponent implements OnInit {
 
   constructor() { }
