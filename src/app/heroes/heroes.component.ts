028467150ff31eca27b6b895092d36d25a222ce2
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-01 16:44:56 -0500

    implemented post method to add hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -26,4 +26,16 @@ export class HeroesComponent implements OnInit {
     .subscribe(heroes => this.heroes = heroes);
   }
 
+  /**When the given name is non-blank the handler creates a Hero-like object from the name and passes it to the services 
+   * addHero() method. When the addHero() saves successfully, the subscribe() callback recieves the new hero and pushes it into to the heroes list
+   * for display */
+  add(name: string): void {
+    name = name.trim();
+    if(!name) {return;}
+    this.heroService.addHero({ name } as Hero)
+        .subscribe(hero => {
+          this.heroes.push(hero);
+        }); 
+  }
+
 }
