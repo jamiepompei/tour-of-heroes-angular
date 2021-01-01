b40077ba8d67ae21020c2fbc5dcb284b86896497
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-01 16:34:47 -0500

    implemented an add button to add a hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -53,7 +53,7 @@ export class HeroService {
   }
 
   /**PUT: update the hero on the server */
-  updateHero(hero: Hero) Observable<any>{
+  updateHero(hero: Hero): Observable<any>{
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
       tap(_ => this.log(`updated hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
