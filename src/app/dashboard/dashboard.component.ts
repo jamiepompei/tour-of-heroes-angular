1bfecfab0b41aed165e87e65a7e1976dd91b57a7
nicolepompei <nicole.lynn.pompei@gmail.com>
2021-01-01 14:10:36 -0500

    added dashboardComponent logic for template and CSS as well as ts logic to return four of the top heroes from the heroes array

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  //defines heroes array property
+  heroes: Hero[] = [];
 
-  constructor() { }
+  //heroService is injected
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  //returns the sliced list of heroes at positions 1 and 5, returning 4 of the tops heroes (pos 2-5)
+  getHeroes(): void{
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 
 }
